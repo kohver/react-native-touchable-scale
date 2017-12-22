cfa7de3efacf6dd9a32a1cf033b6119ce75b0bc5
Guizmo Martinez <martinezguillaume@users.noreply.github.com>
2017-12-22 17:33:04 +0100

    Update prop-types library

--- src/TouchableScale.js
+++ src/TouchableScale.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { TouchableWithoutFeedback, Animated } from 'react-native';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { TouchableWithoutFeedback, Animated } from 'react-native'
 
 /**
  * @typedef {Object} TouchableWithoutFeedbackProps
@@ -34,91 +35,94 @@ import { TouchableWithoutFeedback, Animated } from 'react-native';
  * @property {number} [pressOutFriction]
  */
 export default class TouchableScale extends React.Component {
-    constructor(...args) {
-        super(...args);
-        /** @type {TouchableScaleProps} */
-        const props = this.props;
+  constructor(...args) {
+    super(...args)
+    /** @type {TouchableScaleProps} */
+    const props = this.props
 
-        this.onPressIn = this.onPressIn.bind(this);
-        this.onPressOut = this.onPressOut.bind(this);
-        this.scaleAnimation = new Animated.Value(props.defaultScale);
+    this.onPressIn = this.onPressIn.bind(this)
+    this.onPressOut = this.onPressOut.bind(this)
+    this.scaleAnimation = new Animated.Value(props.defaultScale)
+  }
+
+  render() {
+    /** @type {TouchableScaleProps} */
+    const props = this.props
+
+    return (
+      <TouchableWithoutFeedback
+        // todo: pass only TouchableWithoutFeedback's props.
+        {...props}
+        onPressIn={this.onPressIn}
+        onPressOut={this.onPressOut}>
+        <Animated.View
+          style={[
+            props.style,
+            {
+              transform: [{ scale: this.scaleAnimation }],
+            },
+          ]}>
+          {props.children}
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    )
+  }
+
+  onPressIn(...args) {
+    /** @type {TouchableScaleProps} */
+    const props = this.props
+    const tension =
+      typeof props.pressInTension !== 'undefined' ? props.pressInTension : props.tension
+    const friction =
+      typeof props.pressInFriction !== 'undefined' ? props.pressInFriction : props.friction
+
+    Animated.spring(this.scaleAnimation, {
+      toValue: props.activeScale,
+      tension: tension,
+      friction: friction,
+    }).start()
+
+    if (props.onPressIn) {
+      props.onPressIn(...args)
     }
+  }
 
-    render() {
-        /** @type {TouchableScaleProps} */
-        const props = this.props;
+  onPressOut(...args) {
+    /** @type {TouchableScaleProps} */
+    const props = this.props
+    const tension =
+      typeof props.pressOutTension !== 'undefined' ? props.pressOutTension : props.tension
+    const friction =
+      typeof props.pressOutFriction !== 'undefined' ? props.pressOutFriction : props.friction
 
-        return (
-            <TouchableWithoutFeedback
-                // todo: pass only TouchableWithoutFeedback's props.
-                {...props}
-                onPressIn={this.onPressIn}
-                onPressOut={this.onPressOut}
-            >
-                <Animated.View
-                    style={[props.style, {
-                        transform: [
-                            {scale: this.scaleAnimation},
-                        ]},
-                    ]}
-                >
-                    {props.children}
-                </Animated.View>
-            </TouchableWithoutFeedback>
-        );
+    Animated.spring(this.scaleAnimation, {
+      toValue: props.defaultScale,
+      tension: tension,
+      friction: friction,
+    }).start()
+
+    if (props.onPressOut) {
+      props.onPressOut(...args)
     }
-
-    onPressIn(...args) {
-        /** @type {TouchableScaleProps} */
-        const props = this.props;
-        const tension = typeof props.pressInTension !== 'undefined' ? props.pressInTension : props.tension;
-        const friction = typeof props.pressInFriction !== 'undefined' ? props.pressInFriction : props.friction;
-
-        Animated.spring(this.scaleAnimation, {
-            toValue: props.activeScale,
-            tension: tension,
-            friction: friction,
-        }).start();
-
-        if (props.onPressIn) {
-            props.onPressIn(...args);
-        }
-    }
-
-    onPressOut(...args) {
-        /** @type {TouchableScaleProps} */
-        const props = this.props;
-        const tension = typeof props.pressOutTension !== 'undefined' ? props.pressOutTension : props.tension;
-        const friction = typeof props.pressOutFriction !== 'undefined' ? props.pressOutFriction : props.friction;
-
-        Animated.spring(this.scaleAnimation, {
-            toValue: props.defaultScale,
-            tension: tension,
-            friction: friction,
-        }).start();
-
-        if (props.onPressOut) {
-            props.onPressOut(...args);
-        }
-    }
+  }
 }
 
 TouchableScale.propTypes = {
-    ...TouchableWithoutFeedback.propTypes,
-    style: Animated.View.propTypes.style,
-    defaultScale: React.PropTypes.number.isRequired,
-    activeScale: React.PropTypes.number.isRequired,
-    tension: React.PropTypes.number.isRequired,
-    friction: React.PropTypes.number.isRequired,
-    pressInTension: React.PropTypes.number,
-    pressInFriction: React.PropTypes.number,
-    pressOutTension: React.PropTypes.number,
-    pressOutFriction: React.PropTypes.number,
-};
+  ...TouchableWithoutFeedback.propTypes,
+  style: Animated.View.propTypes.style,
+  defaultScale: PropTypes.number.isRequired,
+  activeScale: PropTypes.number.isRequired,
+  tension: PropTypes.number.isRequired,
+  friction: PropTypes.number.isRequired,
+  pressInTension: PropTypes.number,
+  pressInFriction: PropTypes.number,
+  pressOutTension: PropTypes.number,
+  pressOutFriction: PropTypes.number,
+}
 
 TouchableScale.defaultProps = {
-    defaultScale: 1,
-    activeScale: 0.9,
-    tension: 150,
-    friction: 3,
-};
+  defaultScale: 1,
+  activeScale: 0.9,
+  tension: 150,
+  friction: 3,
+}
